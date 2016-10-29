28391769c0cfd314b7ed9ee1a01656c85b361fe8
Fernando Serapio <me@nosfheratu.xyz>
2016-10-28 23:28:22 -0500

    feature #1: add a to-do item and displayit on the page

--- js/app.js
+++ js/app.js
@@ -1,3 +1,20 @@
 new Vue({
-  el: "#todo"
+  el: "#todo",
+  data: {
+    newTask: "",
+    tasks: []
+  },
+  methods: {
+    addTask: function() {
+      var task = this.newTask.trim();
+      if (task) {
+        this.tasks.push({
+          text: task,
+          checked: false
+        });
+
+        this.newTask = "";
+      }
+    }
+  }
 });
